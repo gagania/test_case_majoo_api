eb254657da4e3982534a2199eaac137945d59f13
gagania <tukangj4l4n@gmail.com>
2021-12-12 22:54:58 +0700

    first commit

---
+++ case3.py
@@ -0,0 +1,11 @@
+
+print('deret pertama')
+x = int(input())
+print('deret kedua')
+y = int(input())
+selisih = y-x
+print('jumlah deret')
+z = int(input())
+
+for x in range(0,z,selisih):
+    print(x)
